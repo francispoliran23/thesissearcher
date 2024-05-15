cd245601895f35e1e39a6e8ece75a37995fafb2a
francispoliran <202180379@psu.palawan.edu.ph>
2024-05-15 13:28:21 +0800

    added pangination errors handler

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Post
 
 
@@ -11,14 +11,21 @@ def post_list(request):
     page_number = request.GET.get('page', 1)
     posts = paginator.page(page_number)
 
+    try:
+        posts = paginator.page(page_number) 
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,
-                  "blog/posts/list.html",
+                  "blog/post/list.html",
                   {"posts":posts})
 
 def post_details(request, year, month, day, post):
     post = get_object_or_404(Post,
                               status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
     return render(request,
-                  "blog/posts/detail.html",
+                  "blog/post/detail.html",
                   {"post":post})
 
